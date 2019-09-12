95d6e57fd87f4d9847c6b8308058f0b3c93b7b54
Scott K Logan <logans@cottsay.net>
2019-09-11 18:40:28 -0700

    WIP

--- test/test_performance.py
+++ test/test_performance.py
@@ -21,6 +21,11 @@ from launch.event_handlers import OnProcessIO
 from launch_ros.actions import Node
 from launch_testing import ActiveIoHandler
 
+import matplotlib  # noqa: F401
+import matplotlib.pyplot as plt
+import numpy as np  # noqa: F401
+import pandas as pd
+
 
 def _create_node(comm='ROS2', topic='Array1k', max_runtime=10, log_file=None):
     return Node(
@@ -29,6 +34,23 @@ def _create_node(comm='ROS2', topic='Array1k', max_runtime=10, log_file=None):
             '-c', comm, '-t', topic, '--max_runtime', str(max_runtime)
         ] + (['-l', log_file] if log_file else []),
     )
+
+
+def _csv_to_png(csv_path):
+    dataframe = pd.read_csv(csv_path, sep='[ \t]*,[ \t]*', engine='python')
+    pd.options.display.float_format = '{:.4f}'.format
+    dataframe['maxrss (Mb)'] = dataframe['ru_maxrss'] / 1e3
+    dataframe.drop(list(dataframe.filter(regex='ru_')), axis=1, inplace=True)
+    dataframe['latency_variance (ms) * 100'] = 100.0 * dataframe['latency_variance (ms)']
+    dataframe[['T_experiment',
+               'latency_min (ms)',
+               'latency_max (ms)',
+               'latency_mean (ms)',
+               'latency_variance (ms) * 100',
+               'maxrss (Mb)']].plot(x='T_experiment', secondary_y=['maxrss (Mb)'])
+
+    png_path = os.path.splitext(csv_path)[0] + '.png'
+    plt.savefig(png_path)
 
 
 class TestRMWPerformance(unittest.TestCase):
@@ -57,8 +79,6 @@ class TestRMWPerformance(unittest.TestCase):
         assert line_start >= 0
 
         print('\n'.join(text_lines[:line_start]))
-        print('PWD: %s' % (os.environ['PWD']))
-        print('CWD: %s' % (os.getcwd()))
 
         csv_path = os.path.join(os.environ['PWD'], 'performance_text_results.csv')
 
@@ -66,3 +86,5 @@ class TestRMWPerformance(unittest.TestCase):
             for line in text_lines[line_start + 1:line_end]:
                 f.write(line)
                 f.write('\n')
+
+        _csv_to_png(csv_path)
