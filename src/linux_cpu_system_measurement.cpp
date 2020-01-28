24940d1bf90e38a5a14f02684b4c5d3cbac0e8ad
ahcorde <ahcorde@gmail.com>
2020-01-28 21:34:15 +0100

    Removed sleep in constructor

--- src/linux_cpu_system_measurement.cpp
+++ src/linux_cpu_system_measurement.cpp
@@ -21,7 +21,6 @@ LinuxCPUSystemMeasurement::LinuxCPUSystemMeasurement()
 {
   initCPUProcess();
   makeReading();
-  std::this_thread::sleep_for(std::chrono::milliseconds(100));
 }
 
 void LinuxCPUSystemMeasurement::initCPUProcess()
