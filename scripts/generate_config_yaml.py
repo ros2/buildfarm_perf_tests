e53c17221ccc39d3310410a39d80fc231f99df78
ahcorde <ahcorde@gmail.com>
2020-03-26 18:38:02 +0100

    fixed flake8
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- scripts/generate_config_yaml.py
+++ scripts/generate_config_yaml.py
@@ -1,4 +1,16 @@
-#!/usr/bin/env python3
+# Copyright 2020 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import argparse
 import os
@@ -8,7 +20,7 @@ from string import Template
 from ament_index_python import get_package_share_directory
 
 
-def add_line_to_plot_overhead(file_output, template_name, label, rmw_implementations, ci_name):
+def add_line_plot_overhead(file_output, template_name, label, rmw_implementations, ci_name):
     """Add line to plot overhead."""
     with open(template_name, 'r') as content_file:
         content = Template(content_file.read())
@@ -77,38 +89,38 @@ def node_spinnnig(file_output, ci_name):
 def overhead_simple_publisher_and_subscriber(file_output, rmw_implementations, ci_name):
     """Generate yaml file for overhead simple publisher and subscriber tests."""
     templates_path = os.path.join(get_package_share_directory('buildfarm_perf_tests'), 'templates')
-    add_line_to_plot_overhead(file_output,
-                              os.path.join(templates_path, 'overhead_round_trip.txt'),
-                              'Overhead simple publisher and subscriber - Average Round-Trip Time:',
-                              rmw_implementations, ci_name)
-    add_line_to_plot_overhead(
+    add_line_plot_overhead(file_output,
+                           os.path.join(templates_path, 'overhead_round_trip.txt'),
+                           'Overhead simple publisher and subscriber - Average Round-Trip Time:',
+                           rmw_implementations, ci_name)
+    add_line_plot_overhead(
                 file_output,
                 os.path.join(templates_path, 'overhead_received_packets.txt'),
                 'Overhead simple publisher and subscriber - Received packets per second:',
                 rmw_implementations,
                 ci_name)
-    add_line_to_plot_overhead(file_output,
-                              os.path.join(templates_path, 'overhead_sent_packets.txt'),
-                              'Overhead simple publisher and subscriber - Sent packets per second:',
-                              rmw_implementations, ci_name)
-    add_line_to_plot_overhead(file_output,
-                              os.path.join(templates_path, 'overhead_lost_packets.txt'),
-                              'Overhead simple publisher and subscriber - Lost packets per second:',
-                              rmw_implementations, ci_name)
-    add_line_to_plot_overhead(file_output,
-                              os.path.join(templates_path, 'overhead_virtual_memory.txt'),
-                              'Overhead simple publisher and subscriber - Virtual Memory:',
-                              rmw_implementations, ci_name)
-    add_line_to_plot_overhead(
+    add_line_plot_overhead(file_output,
+                           os.path.join(templates_path, 'overhead_sent_packets.txt'),
+                           'Overhead simple publisher and subscriber - Sent packets per second:',
+                           rmw_implementations, ci_name)
+    add_line_plot_overhead(file_output,
+                           os.path.join(templates_path, 'overhead_lost_packets.txt'),
+                           'Overhead simple publisher and subscriber - Lost packets per second:',
+                           rmw_implementations, ci_name)
+    add_line_plot_overhead(file_output,
+                           os.path.join(templates_path, 'overhead_virtual_memory.txt'),
+                           'Overhead simple publisher and subscriber - Virtual Memory:',
+                           rmw_implementations, ci_name)
+    add_line_plot_overhead(
                 file_output,
                 os.path.join(templates_path, 'overhead_resident_anonymous_memory.txt'),
                 'Overhead simple publisher and subscriber - Resident Anonymous Memory:',
                 rmw_implementations,
                 ci_name)
-    add_line_to_plot_overhead(file_output,
-                              os.path.join(templates_path, 'overhead_physical_memory.txt'),
-                              'Overhead simple publisher and subscriber - Physical Memory:',
-                              rmw_implementations, ci_name)
+    add_line_plot_overhead(file_output,
+                           os.path.join(templates_path, 'overhead_physical_memory.txt'),
+                           'Overhead simple publisher and subscriber - Physical Memory:',
+                           rmw_implementations, ci_name)
 
 
 def main():
