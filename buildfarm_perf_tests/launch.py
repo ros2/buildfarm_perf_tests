236f3bf48dd3f019601cfd29d4f1f881caffa3c2
Scott K Logan <logans@cottsay.net>
2020-04-24 18:14:35 -0700

    Refactor some Python code into a proper package (#51)
    Signed-off-by: Scott K Logan <logans@cottsay.net>
    Co-authored-by: Alejandro Hernández Cordero <alejandro@openrobotics.org>

---
+++ buildfarm_perf_tests/launch.py
@@ -0,0 +1,58 @@
+# Copyright 2020 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch.actions import RegisterEventHandler
+from launch.actions import SetEnvironmentVariable
+from launch.event_handlers import OnProcessStart
+from launch.substitutions import EnvironmentVariable
+from launch_ros.actions import Node
+
+
+def attach_system_metric_collector(process, log_file, timeout=None):
+    """
+    Receives a Node Class to get the pid and add this id to system_metric_collector.
+
+    Parameters
+    ----------
+    process : Node
+        The Node to get the pid
+    log_file : str
+        path to the log file
+    timeout: int
+         time to keep alive system_metric_collector
+
+    """
+    pid_var_name = 'PROCESS_ID_%d' % id(process)
+
+    args = [
+        '--log', log_file,
+        '--process_pid', EnvironmentVariable(pid_var_name)]
+
+    if timeout is not None:
+        args += ['--timeout', str(timeout)]
+
+    system_metric_collector = Node(
+        package='buildfarm_perf_tests',
+        node_executable='system_metric_collector',
+        arguments=args)
+
+    def on_process_start(process_started, launch_context):
+        SetEnvironmentVariable(
+            pid_var_name, str(process_started.pid)).execute(launch_context)
+        system_metric_collector.execute(launch_context)
+
+    process_start_event = RegisterEventHandler(OnProcessStart(
+        target_action=process, on_start=on_process_start))
+
+    return system_metric_collector, process_start_event
---
+++ buildfarm_perf_tests/test_results.py
@@ -0,0 +1,53 @@
+# Copyright 2020 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pandas import read_csv
+
+
+def read_performance_test_csv(csv_path, start_marker='---EXPERIMENT-START---\n'):
+    """
+    Read CSV data from a file on disk, optionall skipping header data.
+
+    Parameters
+    ----------
+    csv_path : str
+        Path to the *.csv file to read data from.
+    start_marker : str, optional
+        Full line contents which mark the end of the header.
+
+    """
+    with open(csv_path, 'r') as csv:
+        if start_marker:
+            while csv.readline() not in [start_marker, '']:
+                pass
+        return read_csv(csv, sep='[ \t]*,[ \t]*', engine='python')
+
+
+def write_jenkins_plot_csv(csv_path, column_name, values):
+    """
+    Write some values to a CSV file for the Jenkins plot plugin to use.
+
+    Parameters
+    ----------
+    csv_path : str
+        Path to where the *.csv file should be written to.
+    column_name : str
+        Name of this data series, such as the RMW or COMM value.
+    values : list
+        List of string to be written to the file.
+
+    """
+    with open(csv_path, 'w') as csv:
+        csv.write(','.join([column_name] * len(values)) + '\n')
+        csv.write(','.join([str(v) for v in values]) + '\n')
